caf20083d300695f2a27c987d347781c3c5d213b
apomen932-code <apomen932@gmail.com>
2026-03-16 01:48:51 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'archive-cache-v2';
-const FILES = ['./', './index.html', './styles.css', './script.js', './manifest.webmanifest'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).catch(() => {}));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
