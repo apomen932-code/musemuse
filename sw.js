c81371279b5017fd40a058dca42b970f2305657b
apomen932-code <apomen932@gmail.com>
2026-03-16 01:45:17 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE_NAME = 'archive-prototype-v1';
-const ASSETS = ['./', './index.html', './styles.css', './script.js', './manifest.webmanifest'];
-
+const CACHE_NAME = 'archive-cache-v2';
+const FILES = ['./', './index.html', './styles.css', './script.js', './manifest.webmanifest'];
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).catch(() => {}));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
